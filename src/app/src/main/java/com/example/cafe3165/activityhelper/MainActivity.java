7ad5ced5aa0140d7c836de7092e366b203b36b83
Joseph <78599297@qq.com>
2016-11-11 18:25:23 +0800

    change files struct

---
+++ src/app/src/main/java/com/example/cafe3165/activityhelper/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.cafe3165.activityhelper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
